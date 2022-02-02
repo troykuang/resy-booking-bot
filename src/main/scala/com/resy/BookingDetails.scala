a9646193585f9bdc1297b883af886ce1c2c9fa5c
Joe Acosta <jacosta@okcupid.com>
2022-02-02 10:05:50 -0500

    make timing smarter, move custom details

--- src/main/scala/com/resy/BookingDetails.scala
+++ src/main/scala/com/resy/BookingDetails.scala
@@ -2,20 +2,26 @@ package com.resy
 
 object BookingDetails {
   // Your user profile Auth Token
-  val auth_token: String = AuthKeys.auth_token
+  val auth_token: String = CustomDetails.auth_token
+
   // Your user profile API key
-  val api_key: String = AuthKeys.api_key
+  val api_key: String = CustomDetails.api_key
+
   // RestaurantId where you want to make the reservation
-  val venueId: String = AuthKeys.venueId
+  val venueId: String = CustomDetails.venueId
+
   // YYYY-MM-DD of reservation
-  val day: String = AuthKeys.pref_day
+  val day: String = CustomDetails.pref_day
 
   //indoor or outdoor etc. Should match the type exactly on the resy venue (case doesn't matter). Leave blank if you don't care or don't now
-  val dining_type= "Indoor Dining Room"
+  val dining_type: String = CustomDetails.dining_type
 
   // Seq of YYYY-MM-DD HH:MM:SS times of reservations in military time format
-  val times: Seq[String] = AuthKeys.pref_times
+  val times: Seq[String] = CustomDetails.pref_times
 
   // Size of party
   val partySize = "2"
+
+  //Hour of Day to Wake Up the Bot and start searching - In Military Time: 0-23
+  val hourOfDayToStartBooking: Int = CustomDetails.hourOfDayToStartBooking
 }
--- src/main/scala/com/resy/ResyBookingBot.scala
+++ src/main/scala/com/resy/ResyBookingBot.scala
@@ -2,6 +2,7 @@ package com.resy
 
 import akka.actor.ActorSystem
 import com.resy.BookReservationWorkflow._
+import com.resy.BookingDetails.hourOfDayToStartBooking
 import org.joda.time.DateTime
 import play.api.libs.json._
 
@@ -15,26 +16,35 @@ object ResyBookingBot{
   def main(args: Array[String]): Unit = {
     println("Starting Resy Booking Bot")
 
-    val system              = ActorSystem("System")
-    val startOfTomorrow     = DateTime.now.withTimeAtStartOfDay.plusDays(1).getMillis
-    val millisUntilTomorrow = startOfTomorrow - DateTime.now.getMillis - 1000
-    val hoursRemaining      = millisUntilTomorrow / 1000 / 60 / 60
-    val minutesRemaining    = millisUntilTomorrow / 1000 / 60 - hoursRemaining * 60
+    val system = ActorSystem("System")
+    val rightNow = DateTime.now.hourOfDay().get
+    val hourToStartChecking = hourOfDayToStartBooking
+
+    val startCheckTime =
+      if (rightNow < hourToStartChecking) {
+        DateTime.now.withTimeAtStartOfDay().plusHours(hourToStartChecking).getMillis
+      } else {
+        DateTime.now.withTimeAtStartOfDay().plusDays(1).plusHours(hourToStartChecking).getMillis
+      }
+
+    val millisUntilStart = startCheckTime - DateTime.now.getMillis - 1000
+    val hoursRemaining = millisUntilStart / 1000 / 60 / 60
+    val minutesRemaining = millisUntilStart / 1000 / 60 - hoursRemaining * 60
     val secondsRemaining =
-      millisUntilTomorrow / 1000 - hoursRemaining * 60 * 60 - minutesRemaining * 60
+      millisUntilStart / 1000 - hoursRemaining * 60 * 60 - minutesRemaining * 60
 
     println(s"Current time: ${DateTime.now}")
     println(
       s"Sleeping for $hoursRemaining hours, $minutesRemaining minutes and $secondsRemaining seconds"
     )
-    system.scheduler.scheduleOnce(millisUntilTomorrow millis)(bookReservationWorkflow)
+    system.scheduler.scheduleOnce(millisUntilStart millis)(bookReservationWorkflow)
   }
 
   def bookReservationWorkflow = {
     println(s"Attempting to snipe reservation at ${DateTime.now}")
 
     //Try to get configId of the time slot for 10 seconds
-    val findResResp = retryFindReservation(DateTime.now.plusSeconds(10).getMillis)
+    val findResResp = retryFindReservation(DateTime.now.plusSeconds(15).getMillis)
 
     //Try to book the reservation
     for {
